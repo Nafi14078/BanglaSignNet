95f95425fcd0e9344d968a27c590d3ae5f33056d
Ashfak Azad Nafi <ashfakazad@iut-dhaka.edu>
2025-11-03 23:15:59 +0600

    Initial commit: Professional BanglaSignNet project structure done

---
+++ src/utils/config.py
@@ -0,0 +1,11 @@
+﻿import yaml
+import os
+
+def load_config(config_path=\"config.yaml\"):
+    with open(config_path, 'r') as file:
+        config = yaml.safe_load(file)
+    return config
+
+def save_config(config, config_path=\"config.yaml\"):
+    with open(config_path, 'w') as file:
+        yaml.dump(config, file, default_flow_style=False)
